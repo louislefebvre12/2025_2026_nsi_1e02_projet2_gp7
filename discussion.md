93843a19aec0cc05c00f6905789dcaf3afc70edb
louislefebvre12 <louislefebvre12@icloud.com>
2025-12-09 17:06:39 +0100

    ajout tres important dans discussion et création de graph dans test

--- discussion.md
+++ discussion.md
@@ -14,4 +14,7 @@ data.columns afficher les colonnes du dossier avec les titres
 data.head() afficher les premieres lignes du dossier
 data.describe() statistiques de bases des colonnes du dossier
 data.fillna(data['column'].mean()) remplacer des données manquantes par des données par défaut(la moyenne ici) !!!! corruption du data set !!!!
-data.dropna(axis = 0)  éliminer les lignes dans lesquelles il nous manque des données dommage
+data.dropna(axis = 0)  éliminer les lignes dans lesquelles il nous manque des données change les stats
+data['column'].value_counts() donne les infos de la colonnne sélectionnée
+data['column'].value_counts().plot.bar() fait un graphique avec des barres des infos de la colonne sélectionnée !!!! ne pas oublier plt.show() 
+plt.show() affiche dans une nouvelle fenetre le graphique matplotlib
--- test.py
+++ test.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 data = pd.read_excel('fr-en-baccalaureat-par-departement.xlsx')
+print(data)
+print("=============================================")
+# data = pd.DataFrame(data)
 print(data.shape)
 print("=============================================")
 print(data.columns)
@@ -10,3 +13,10 @@ print("=============================================")
 print(data.head())
 print("=============================================")
 print(data.describe())
+print("=============================================")
+print(data["Code académie"].value_counts())
+print("=============================================")
+print(data["Code académie"].value_counts().plot.bar())
+print("=============================================")
+# plt.plot( data["Nombre de présents à l'examen"], data["Code académie"], ) peut servir pour comparer deux colonnes
+plt.show ()
