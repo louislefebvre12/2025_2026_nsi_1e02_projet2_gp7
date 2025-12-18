06a5369d8120e95e6a1a686314779d0ca03094a4
alex-jga <jaouena8@gmail.com>
2025-12-18 12:10:02 +0100

    TUTORIAL Tkinter

--- testing_tkinter.py
+++ testing_tkinter.py
@@ -1,33 +1,10 @@
-from tkinter import *
+from tkinter import * 
 
-root = Tk()
+window = Tk()
 
-# specify size of window.
-root.geometry("250x170")
+label = Label(window, text = "Text", font = ('Arial' , 40, 'bold'), fg = "#00FF00")
 
-# Create text widget and specify size.
-T = Text(root, height = 5, width = 52)
+#label.pack() ou label.place(x = 0 , y = 0)
+label.pack()
 
-# Create label
-l = Label(root, text = "Fact of the Day")
-l.config(font =("Courier", 14))
-
-Fact = """A man can be arrested in
-Italy for wearing a skirt in public."""
-
-# Create button for next text.
-b1 = Button(root, text = "Next", )
-
-# Create an Exit button.
-b2 = Button(root, text = "Exit",
-            command = root.destroy) 
-
-l.pack()
-T.pack()
-b1.pack()
-b2.pack()
-
-# Insert The Fact.
-T.insert(END, Fact)
-
-mainloop()
+window.mainloop()   
