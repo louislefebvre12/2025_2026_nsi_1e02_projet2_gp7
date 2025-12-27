93ebb08ec13cb07fbbcd6021702dccfc15d4ae9f
alex-jga <jaouena8@gmail.com>
2025-12-27 11:34:40 +0100

    Mode d'emploi Tkinter + Début fenêtre TKinter

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from tkinter import *
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 
+root = Tk()
+root.title("Résultats du Baccalauréat par Département")
 data = pd.read_excel('fr-en-baccalaureat-par-departement.xlsx')
+my_label = Label (root, text = "Commencer" )
+my_label.pack()
+root.mainloop()
