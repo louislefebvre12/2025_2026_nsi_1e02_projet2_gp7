b6cc164d6823eefc52dcbc9f5e8f0cf7a205c736
alex-jga <jaouena8@gmail.com>
2026-01-27 15:43:56 +0100

    creation data

--- main.py
+++ main.py
@@ -6,6 +6,20 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
 
+data_A = {'x': [1, 2, 3, 4, 5], 'y': [1, 4, 9, 16, 25]}
+data_B = {'x': [1, 2, 3, 4, 5], 'y': [5, 4, 3, 2, 1]} 
+data_C = {'x': [1, 2, 3, 4, 5], 'y': [1, 2, 3, 4, 5]} 
+data_D = {'x': [1, 2, 3, 4, 5], 'y': [5, 10, 15, 20, 25]}  
+
+
+dfs = {
+    'A': pd.DataFrame(data_A),
+    'B': pd.DataFrame(data_B),
+    'C': pd.DataFrame(data_C),
+    'D': pd.DataFrame(data_D)
+}
+
+
 def plot() : 
     ax.clear()
 
