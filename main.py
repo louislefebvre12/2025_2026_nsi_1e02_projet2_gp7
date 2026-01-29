6089a0303fe84dfd70708d6a7a7906c732e119f4
alex-jga <jaouena8@gmail.com>
2026-01-29 12:01:48 +0100

    remove tk and tkk

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tkinter as tk
-from tkinter import ttk
+
 from tkinter import messagebox
 from tkinter import *
 import pandas as pd
@@ -8,14 +7,14 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
-class Extra(tk.Toplevel):
+class Extra(Toplevel):
 	def __init__(self):
 		super().__init__()
 		self.title('extra window')
 		self.geometry('300x400')
-		ttk.Label(self, text = 'A label').pack()
-		ttk.Button(self, text = 'A button').pack()
-		ttk.Label(self, text = 'another label').pack(expand = True)
+		Label(self, text = 'A label').pack()
+		Button(self, text = 'A button').pack()
+		Label(self, text = 'another label').pack(expand = True)
 		self.plot_window ()
 	def plot_window(self):
 		root = Tk()
@@ -36,10 +35,9 @@ class Extra(tk.Toplevel):
 		root.mainloop()
 	
 
-# https://docs.python.org/3/library/tkinter.messagebox.html
+
+
 def ask_yes_no():
-	# answer = messagebox.askquestion('Title', 'Body')
-	# print(answer)
 	messagebox.showerror('Info title', 'Here is some information')
 
 def create_window():
@@ -50,19 +48,18 @@ def create_window():
 def close_window():
 	exit()
 
-# window
-window = tk.Tk()
+
+window = Tk()
 window.geometry('600x400')
 window.title('Multiple windows')
 
-button1 = ttk.Button(window, text = 'Open main window', command = create_window)
+button1 = Button(window, text = 'Open main window', command = create_window)
 button1.pack(expand = True)
 
-button2 = ttk.Button(window, text = 'Close main window', command = close_window)
+button2 = Button(window, text = 'Close main window', command = close_window)
 button2.pack(expand = True)
 
-button3 = ttk.Button(window, text = 'create yes no window', command = ask_yes_no)
+button3 = Button(window, text = 'create yes no window', command = ask_yes_no)
 button3.pack(expand = True)
 
-# run 
 window.mainloop()
