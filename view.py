dd579c21d9da13c1c3b75191aebe05815d9ae048
alex-jga <jaouena8@gmail.com>
2026-02-03 16:53:11 +0100

    creation of multiple classes
    Such as view, controller, and model ...

---
+++ view.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from tkinter import ttk
+
+class View:
+    def __init__ (self, root):
+        self.root = root
+        self.root.title("Gestion des Noms et Prénoms")
+        self.root.geometry("500x400")
