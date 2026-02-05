37cb6bcfb4a156f8f29b096301b4e2d9773734b3
alex-jga <jaouena8@gmail.com>
2026-02-05 12:10:40 +0100

    new fonctions for Controller

--- controller.py
+++ controller.py
@@ -1,3 +1,12 @@
+
+from tkinter import messagebox
+from tkinter import *
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
 from model import Model
 from view import View
 
@@ -7,13 +16,43 @@ class Controller:
         self.model = model
         self.view = view
 
-        chaines = self.model.charger_chaines()
-        self.view.actualiser_listbox(chaines)
-
-    def valider_chaine(self, chaine):
+        self.view.self.button1.config(command=self.close_window)
+        self.view.self.button2.config(command=self.close_window)
+        self.view.self.button3.config(command=self.close_window)
+        self.view.self.button4.config(command=self.close_window)
         
-    def action_valider(self):
+        def close_window() :
+            view.destroy()
         
+        def prediction_window ():
+            pass
 
-    def action_supprimer(self):
-        self.window.quit()
+        def ranking_window() : 
+            pass
+
+        def plot_window() : 
+            top = Toplevel(self)
+            top.title("Résultats au BAC")
+            top.geometry("900x600")
+            fig, ax = plt.subplots(figsize=(6, 4))
+            canvas = FigureCanvasTkAgg(fig, master=top)
+            label = Label(
+			top,
+			text='Résultats au BAC',
+			padx=10,
+			pady=10,
+			borderwidth=2,
+			relief='solid',
+			font=("Courier", 24)
+		)
+            label.pack(pady=10)
+            frame = Frame(top)
+            frame.pack(pady=10)
+            for i in range(1, 7):
+                Button(
+				frame,
+				text=f'Statistiques {i}',
+				command=lambda i=i: print(f"Stat {i}")
+			).pack(side=LEFT, padx=5)
+                canvas.get_tk_widget().pack(expand=True, fill=BOTH)
+
--- view.py
+++ view.py
@@ -1,8 +1,33 @@
-import tkinter as tk
-from tkinter import ttk
+
+from tkinter import *
 
 class View:
-    def __init__ (self, root):
-        self.root = root
-        self.root.title("Gestion des Noms et Prénoms")
-        self.root.geometry("500x400")
+    def __init__ (self, window):
+        self.window = window
+        self.window.title("Gestion des Noms et Prénoms")
+        self.window.geometry("500x400")
+        self.window.geometry('600x400')
+        self.window.title('Multiple windows')
+
+        self.button1 = Button(window, text = 'Open main window', command = plot_window(self))
+        self.button1.pack(expand = True)
+
+        self.button2 = Button(window, text = 'Close main window', command = ranking_window(self))
+        self.button2.pack(expand = True)
+
+        self.button3 = Button(window, text = 'PRÉDICTION', command = prediction_window)
+        self.button3.pack(expand = True)
+        
+        self.button4 = Button(window, text = 'QuiT', command = close_window)
+        self.button4.pack(expand = True)
+
+        def close_window() :
+            window.destroy()
+
+
+
+
+
+
+
+
