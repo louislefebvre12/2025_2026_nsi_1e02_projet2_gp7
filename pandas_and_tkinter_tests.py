c11a00649c160c97ca83e5d6d6b47926fcdb5789
boogiekalt <bogdan.kaltrachian@gmail.com>
2026-02-08 22:34:46 +0100

    test histogramme pandas et tkinter

--- pandas_and_tkinter_tests.py
+++ pandas_and_tkinter_tests.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import seaborn as sns
+import tkinter as tk
 
 print ("hello !")
 
@@ -34,4 +35,18 @@ plt.ylabel ("Code académie")
 # plt.plot( data["Nombre de présents à l'examen"], data["Code académie"], ) peut servir pour comparer deux colonnes
 sns.barplot(data=data, x='Code département', y="Taux de réussite à l'examen", hue='Genre')
 plt.title('diagramme session code académie et genre')
+root = tk.Tk()
+root.title("Histogramme du taux de réussite à l'examen par académie")
+root.geometry("600x400")
+
+fig, ax = plt.subplots(figsize=(6, 4))
+ax.hist(data["Taux de réussite à l'examen"], bins=5, color="skyblue", edgecolor="black")
+
+ax.set_title("Histogramme")
+ax.set_xlabel("Code académie")
+ax.set_ylabel("Taux de réussite à l'examen")
+
+canvas = FigureCanvasTkAgg(fig, master=root)
+canvas.draw()
+canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
 plt.show ()
