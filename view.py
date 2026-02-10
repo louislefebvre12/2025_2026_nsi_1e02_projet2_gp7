8035ef3dc5374dcddcd92dc2ce1d68a01c245ef0
alex-jga <jaouena8@gmail.com>
2026-02-10 16:13:21 +0100

    clean

--- view.py
+++ view.py
@@ -9,25 +9,17 @@ class View:
         self.window.geometry('600x400')
         self.window.title('Multiple windows')
 
-        self.button1 = Button(window, text = 'Open main window', command = plot_window(self))
+        self.button1 = Button(window, text = 'Open main window')
         self.button1.pack(expand = True)
 
-        self.button2 = Button(window, text = 'Close main window', command = ranking_window(self))
+        self.button2 = Button(window, text = 'Close main window')
         self.button2.pack(expand = True)
 
-        self.button3 = Button(window, text = 'PRÉDICTION', command = prediction_window)
+        self.button3 = Button(window, text = 'PRÉDICTION')
         self.button3.pack(expand = True)
         
-        self.button4 = Button(window, text = 'QuiT', command = close_window)
+        self.button4 = Button(window, text = 'QuiT')
         self.button4.pack(expand = True)
-
-        def close_window() :
-            window.destroy()
-
-
-
-
-
 
 
 
