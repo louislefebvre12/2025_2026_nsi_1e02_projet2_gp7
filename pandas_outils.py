6526074f4e3503a84faa881a2c0c56bcb074a6ad
louislefebvre12 <louislefebvre12@icloud.com>
2026-02-10 17:08:23 +0100

    rajout du camembew a corriger car 50/50

--- pandas_outils.py
+++ pandas_outils.py
@@ -25,54 +25,54 @@ print("=============================================")
 print(data.groupby("Genre").mean(numeric_only=True))
 print("=============================================")
 #Diagramme de dispersion a corriger
-#y = data["Code académie"]
-#x = data["Genre"]
-#fig = plt.figure(figsize=(20,100))
-#plt.scatter(x,y)
-#plt.title ("Test")
-#plt.xlabel ("Axe x")
-#plt.ylabel ("Axe y")
-#plt.plot( data["Genre"], data["Code académie"], ) #peut servir pour comparer deux colonnes
+y = data["Code académie"]
+x = data["Genre"]
+fig = plt.figure(figsize=(20,100))
+plt.scatter(x,y)
+plt.title ("Test")
+plt.xlabel ("Axe x")
+plt.ylabel ("Axe y")
+plt.plot( data["Genre"], data["Code académie"], ) #peut servir pour comparer deux colonnes
 print("=============================================")
 #Tracé linéaire a corriger
-# AGGREGATION = "mean"
-# data = pd.read_excel("fr-en-baccalaureat-par-departement.xlsx")
-# data.columns = (
-#     data.columns
-#     .str.strip()
-# )
-# data["Taux de réussite à l'examen"] = (
-#     data["Taux de réussite à l'examen"]
-#     .astype(str)
-#     .str.replace('%', '', regex=False)
-#     .str.replace(',', '.', regex=False)
-# )
-# data["Taux de réussite à l'examen"] = pd.to_numeric(data["Taux de réussite à l'examen"], errors='coerce')
-# if AGGREGATION == "mean":
-#     df = data.groupby("Code académie", as_index=False)["Taux de réussite à l'examen"].mean()
-# else:
-#     raise ValueError("Probleme de donnés")
-# df = df.sort_values("Code académie")
-# root = tk.Tk()
-# root.title("Courbe du taux de réussite par académie")
-# root.geometry("1200x800")
-# fig, ax = plt.subplots(figsize=(10, 6))
-# sns.lineplot(
-#     data=df,
-#     x="Code académie",
-#     y="Taux de réussite à l'examen",
-#     marker="o",
-#     ax=ax
-# )
-# ax.set_title("Courbe du taux de réussitepar académie")
-# ax.set_xlabel("Code académie")
-# ax.set_ylabel("Taux de réussite à l'examen à l'examen")
-# ax.grid(True)
-# plt.xticks(rotation=45)
-# plt.tight_layout()
-# canvas = FigureCanvasTkAgg(fig, master=root)
-# canvas.draw()
-# canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+aggregation_trace = "mean"
+data = pd.read_excel("fr-en-baccalaureat-par-departement.xlsx")
+data.columns = (
+    data.columns
+    .str.strip()
+)
+data["Taux de réussite à l'examen"] = (
+    data["Taux de réussite à l'examen"]
+    .astype(str)
+    .str.replace('%', '', regex=False)
+    .str.replace(',', '.', regex=False)
+)
+data["Taux de réussite à l'examen"] = pd.to_numeric(data["Taux de réussite à l'examen"], errors='coerce')
+if aggregation_trace == "mean":
+    df = data.groupby("Code académie", as_index=False)["Taux de réussite à l'examen"].mean()
+else:
+    raise ValueError("Probleme de donnés")
+df = df.sort_values("Code académie")
+root = tk.Tk()
+root.title("Courbe du taux de réussite par académie")
+root.geometry("1200x800")
+fig, ax = plt.subplots(figsize=(10, 6))
+sns.lineplot(
+    data=df,
+    x="Code académie",
+    y="Taux de réussite à l'examen",
+    marker="o",
+    ax=ax
+)
+ax.set_title("Courbe du taux de réussitepar académie")
+ax.set_xlabel("Code académie")
+ax.set_ylabel("Taux de réussite à l'examen à l'examen")
+ax.grid(True)
+plt.xticks(rotation=45)
+plt.tight_layout()
+canvas = FigureCanvasTkAgg(fig, master=root)
+canvas.draw()
+canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
 
 print("=============================================")
 #Histogramme a corriger
@@ -92,9 +92,30 @@ canvas.draw()
 canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
 print("=============================================")
 #Camembert a corriger
-#data = data('')['Genre'].count().reset_index()
-#plt.pie(data['Genre'], labels=data['Code académie'])
-#plt.title('Camembert code académie et genre')
+aggregation_camembert = "sum" 
+TITLE = "Répartition par genre"
+data = pd.read_excel("fr-en-baccalaureat-par-departement.xlsx")
+data.columns = data.columns.str.strip()
+if aggregation_camembert == "sum":
+    df = data.groupby("Genre", as_index=False)["Session"].sum()
+else:
+    raise ValueError("Probleme de donnés")
+root = tk.Tk()
+root.title("Répartition par genre")
+root.geometry("900x700")
+fig, ax = plt.subplots(figsize=(12, 12))
+ax.pie(
+    df["Session"],
+    labels=df["Genre"],
+    autopct='%1.1f%%',
+    startangle=90
+)
+ax.set_title("Répartition par genre")
+ax.axis('equal')  # cercle parfait
+plt.tight_layout()
+canvas = FigureCanvasTkAgg(fig, master=root)
+canvas.draw()
+canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
 print("=============================================")
 #Diagramme à barres groupées a corriger
 #sns.barplot(data=data, x='Voie', y='Session', hue='Genre')
